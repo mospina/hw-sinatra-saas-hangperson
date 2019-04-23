1b6fb94833ce9664542ff6979ea971d0e5b38200
Manuel Ospina <manuel@anect.jp>
2019-04-23 17:12:20 +0900

    Implement methods for hangperson game according to test.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,6 +2,7 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses
 
   # Get a word from remote "random word" service
 
@@ -10,6 +11,32 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    raise ArgumentError, "#{letter} is not valid." unless /[a-zA-Z]/.match(letter)
+
+    letter = letter.downcase 
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    if @word.include? letter
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+  end
+
+  def word_with_guesses
+    @word.chars.map { |letter| guesses.include?(letter) ? letter : '-' }.join
+  end
+
+  def check_win_or_lose
+    return :lose if @wrong_guesses.length >= 7
+
+    result = @word.chars.map { |letter| guesses.include?(letter) ? true : false }.all?
+    result ? :win : :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
