bad975d379ce6d828b0cf23dc657c9e4bba67666
Manuel Ospina <manuel@anect.jp>
2019-04-24 15:10:38 +0900

    Complete features for app

--- app.rb
+++ app.rb
@@ -40,7 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    flash[:message] = "You have already used that letter." unless @game.guess(letter)
+
     redirect '/show'
+
+  rescue ArgumentError
+    flash[:message] = "Invalid guess"
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
@@ -50,16 +55,27 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose 
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :win
+
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    redirect '/show' unless @game.check_win_or_lose == :lose
+
     erb :lose # You may change/remove this line
   end
   
